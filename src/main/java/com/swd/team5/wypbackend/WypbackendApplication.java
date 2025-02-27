5d05c14d2d8f9739b1040f3b2a18d534ca0d20d0
tridovan <tridvse173191@fpt.edu.vn>
2025-02-27 09:10:45 +0700

    init app

---
+++ src/main/java/com/swd/team5/wypbackend/WypbackendApplication.java
@@ -0,0 +1,13 @@
+package com.swd.team5.wypbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WypbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WypbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swd/team5/wypbackend/WypbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swd.team5.wypbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WypbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
